45c3bfef313ac0e9cce6ec68695020feb6bf733a
IWT <admin@iwebtouch.com>
2020-04-30 16:06:24 +0700

    remove packages from npm, put assets to public directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js');
+//    .extract(['axios', 'lodash', 'vue','jquery', 'bootstrap']);
+mix.sass('resources/sass/app.scss', 'public/css');
+
+
