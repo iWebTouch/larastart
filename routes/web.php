66cda19c59b209322fb2f08fd940272c2d8abb79
IWT <admin@iwebtouch.com>
2020-05-02 12:28:53 +0700

    Add routes for managing users

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,23 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware('admin')->namespace('Admin')->group(function() {
-    Route::get('/admin', 'AdminController@index')->name('admin.home');
-});
-
 Route::middleware('auth')->group(function() {
     Route::get('/home', 'HomeController@index')->name('user.home');
+});
+
+Route::prefix('/admin')->middleware('admin')->namespace('Admin')->group(function() {
+
+    Route::get('/', 'AdminController@index')->name('admin.home');
+
+    Route::prefix('users')->group(function() {
+        Route::get('/', 'UserController@index')->name('manage.users');
+        Route::get('/data', 'UserController@data')->name('users.data');
+
+        Route::get('/new', 'UserController@add')->name('add.user');
+        Route::post('/new', 'UserController@store')->name('create.user');
+        Route::get('/{id}', 'UserController@edit')->name('edit.user');
+        Route::put('/{id}', 'UserController@update')->name('update.user');
+        Route::delete('/{id}', 'UserController@destroy')->name('delete.user');  
+    });
+
 });
