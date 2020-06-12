c36e314aae0688bb0e10429d161758de1476d294
IWT <admin@iwebtouch.com>
2020-06-12 17:58:25 +0700

    create seeder for admin user

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        // create admin user
+        factory(App\User::class)->create([
+            'name' => 'Administrator',
+            'email' => 'admin@site.com',
+            'is_admin' => 1
+        ]);
+
+    }
+}
