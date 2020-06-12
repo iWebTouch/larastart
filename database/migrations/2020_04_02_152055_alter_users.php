852e49820bd6822548c9eb06382cdd3983c13715
IWT <admin@iwebtouch.com>
2020-06-12 17:27:10 +0700

    fix migration

--- database/migrations/2020_04_02_152055_alter_users.php
+++ database/migrations/2020_04_02_152055_alter_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterUsersRole extends Migration
+class AlterUsers extends Migration
 {
     /**
      * Run the migrations.
