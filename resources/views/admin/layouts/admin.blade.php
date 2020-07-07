29ad942f651e5ad86f8c6f59dc05d886be8a8d46
IWT <admin@iwebtouch.com>
2020-07-07 10:50:09 +0700

    change footer text

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -87,7 +87,7 @@
                 <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Vote App 2020</span>
+                    <span>Copyright &copy; LaraStart 2020</span>
                     </div>
                 </div>
                 </footer>
