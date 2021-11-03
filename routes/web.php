56055a29416fb7b7a8ae7550952d48197f6eb0a0
IWT <admin@iwebtouch.com>
2021-11-03 08:25:09 +0700

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
+
+Route::middleware('auth')->group(function() {
+    Route::get('/home', 'HomeController@index')->name('user.home');
+
+    Route::get('/profile/edit', 'UserController@edit')->name('edit.profile');
+    Route::put('/profile/edit', 'UserController@update')->name('update.profile');
+    Route::get('/profile/{id}', 'UserController@show')->name('show.profile');
+
+});
+
+Route::prefix('/admin')->middleware('admin')->namespace('Admin')->group(function() {
+
+    Route::get('/', 'AdminController@index')->name('admin.home');
+    Route::get('/settings', 'AdminController@settings')->name('app.settings');
+    Route::put('/settings', 'AdminController@updateSettings')->name('update.settings');
+
+    Route::prefix('users')->group(function() {
+        Route::get('/', 'UserController@index')->name('manage.users');
+        Route::get('/data', 'UserController@data')->name('users.data');
+
+        Route::get('/new', 'UserController@add')->name('add.user');
+        Route::post('/', 'UserController@store')->name('create.user');
+        Route::get('/{user}/edit', 'UserController@edit')->name('edit.user');
+        Route::put('/{user}', 'UserController@update')->name('update.user');
+        Route::delete('/{user}', 'UserController@destroy')->name('delete.user');
+    });
+
+});
