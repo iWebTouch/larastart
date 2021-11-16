48d3aa6189303243c5b4c29d1dd8f37c4d98a0c1
xdimension <xdim222@gmail.com>
2021-11-16 17:55:49 +0700

    move models to Models directory

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -1,10 +1,12 @@
 <?php
+namespace App\Models;
 
-namespace App;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    use HasFactory;
+    
     protected $fillable = ['name', 'email', 'logo'];
 }
